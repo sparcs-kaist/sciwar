a56559b78d2c66538f443d2f0fee89ddf3ac4588
argon <argon@sparcs.org>
2013-09-24 05:41:50 +0900

    settings modified

--- sciwar/sciwar/settings.py
+++ sciwar/sciwar/settings.py
@@ -31,7 +31,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Asia/Seoul'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -52,12 +52,13 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = PROJECT_PATH + '/main/media/'
+MEDIA_ROOT = PROJECT_PATH + '/../main/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = PROJECT_PATH + '/main/media/'
+MEDIA_URL = PROJECT_PATH + '/../main/media/'
+
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
