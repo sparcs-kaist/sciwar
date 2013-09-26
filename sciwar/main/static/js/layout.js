1ad94bd5eb5300326709e040797c06ac796b6698
rodumani <rodumani@sparcs.org>
2013-09-27 00:00:49 +0900

    Players information

--- sciwar/main/static/js/layout.js
+++ sciwar/main/static/js/layout.js
@@ -4,6 +4,8 @@ var Creators = {
         this.developers = $("#creators>.developers>.list");
         this.btn_designers = $("#creators>.designers>button");
         this.designers = $("#creators>.designers>.list");
+		this.btn_players = $(".live>.players>button");
+		this.players = $(".live>.players>.players-table");	
 
         this.registerHandlers();
     },
@@ -23,6 +25,15 @@ var Creators = {
                 $(this).removeClass("flip-hide").addClass("flip-show");
             }else{
                 $(Creators.designers).show('fast');
+                $(this).removeClass("flip-show").addClass("flip-hide");
+            }
+        });
+        $(this.btn_players).click(function(){
+            if($(this).hasClass("flip-hide")){
+                $(Creators.players).slideToggle('slow');
+                $(this).removeClass("flip-hide").addClass("flip-show");
+            }else{
+                $(Creators.players).slideToggle('slow');
                 $(this).removeClass("flip-show").addClass("flip-hide");
             }
         });
